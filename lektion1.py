e55e69d2863c22e820f10d47f96fce938d4ca25b
TatevikTadevosyan86 <151789047+TatevikTadevosyan86@users.noreply.github.com>
2024-06-23 21:23:15 +0200

    Add files via upload

---
+++ lektion1.py
@@ -0,0 +1,16 @@
+
+def list(lista):
+    nylista =[]
+    for i in lista:
+        nyttelement = pow(i,2)
+        nylista.append(nyttelement)
+    return nylista
+print(list([2,4,6,7,8]))
+    
+
+
+
+
+
+
+
